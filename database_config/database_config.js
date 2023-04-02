438c26af22ad68878385c2302e31d44391c20ce1
lalon147 <lalon.rahman147@gmail.com>
2023-04-03 03:40:19 +0600

    changes

--- database_config/database_config.js
+++ database_config/database_config.js
@@ -4,7 +4,17 @@ require("dotenv").config();
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gw8hef2.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const sellersCollection = client.db("post-master").collection("sellers");
+ 
 
+
+
+
+
+
+module.exports={
+    sellersCollection
+}
 
 
 
--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors=require('cors');
-
+const { sellersCollection } = require('./database_config/database_config');
 const app=express()
 require("dotenv").config();
 app.use(cors())
 app.use(express.json())
 
-
 const port =  process.env.PORT || 5000
-app.get('/', (req,res) => {
-      res.send("SErver is running")
+
+
+
+
+app.get('/', async (req,res) => {
+    const sellers=await sellersCollection.find({}).toArray()
+    res.send(sellers);
 })
+
+
 
 
 
