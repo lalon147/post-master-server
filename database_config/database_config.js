2743eb7b86c2fb9901210edbd972b26eeee8db22
shafikul-islam <179398ssshofik@gmail.com>
2023-04-03 22:24:36 +0600

    check the branch is working or not

--- database_config/database_config.js
+++ database_config/database_config.js
@@ -2,6 +2,8 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 
  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gw8hef2.mongodb.net/?retryWrites=true&w=majority`;
+ 
+ console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const sellersCollection = client.db("post-master").collection("sellers");
